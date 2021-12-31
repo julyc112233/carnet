d0a66012efd4e2ee7b5f2ccf619627e8f560091d
zeng weijia <weijia.zeng@aifi.com>
2021-12-31 11:28:48 +0800

    添加了carnet 可视化
    添加了stn_shuf,stn_trans_shuf两个网络

--- eval.py
+++ eval.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from model.malexnet import mAlexNet
 from model.alexnet import AlexNet
+from model.trans_shuf import trans_shufflenet
 from model.carnet import carNet
 from model.stn_shuf import stn_shufflenet
 from model.stn_trans_shuf import stn_trans_shufflenet
@@ -44,11 +45,41 @@ def eval(img_path,target_path, net,str="rainy"):
     FP=0
     FN=0
     TN=0
+    count = [0 for x in range(0,200)]
+    # count['_88']=1
+    # count['_88']=count['_88']+1
+    # print(count)
+    # exit()
+    # f=open('89.250.150.72.txt','w')
     with torch.no_grad():
-        for i in tqdm(range(test_size)):
+        for i in range(test_size):
+            # for ind,val in enumerate(count):
+            #     print(ind,val)
             split_path = args.split_path
             split_path = osp.join(split_path, str)
             image=test_dataset.pull_img(i)
+            # print(test_dataset.img_list[i])
+            # if test_dataset.img_list[i].find("_25")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_26")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_27")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_30")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_119") >= 0:
+            #     continue
+            # if test_dataset.img_list[i].find("_111")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_122")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_150")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_107")>=0:
+            #     continue
+            # if test_dataset.img_list[i].find("_105")>=0:
+            #     continue
+            # print(test_dataset.img_list[i].find("_76.jpg"))
             if image is None:
                 continue
             tmp_image=test_dataset.pull_img(i)
@@ -67,27 +98,41 @@ def eval(img_path,target_path, net,str="rainy"):
             # exit()
             total += 1
             correct+=(predicted==label)
+
+            # if predicted!=label:
+            #     print(predicted, test_dataset.img_list[i])
+            #     index=int(test_dataset.img_list[i].split('_')[-1].split('.')[0])
+            #     count[index]=count[index]+1
+                # cv2.imshow('test',tmp_image)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()
             if predicted == 1 and label == 1:
                 TP += 1
                 split_path = osp.join(split_path, "TP")
             elif predicted == 1 and label == 0:
                 FP += 1
                 split_path = osp.join(split_path, "FP")
+                # f.write(test_dataset.img_list[i]+'\n')
+                # print(predicted,test_dataset.img_list[i])
             elif predicted == 0 and label == 1:
                 FN += 1
                 split_path = osp.join(split_path, "FN")
+                # print(predicted,test_dataset.img_list[i])
             elif predicted==0 and label ==0:
                 TN += 1
                 split_path = osp.join(split_path, "TN")
-            if not os.path.exists(split_path):
-                os.makedirs(split_path)
-            split_path=osp.join(split_path,repr(i)+".jpg")
-            cv2.imwrite(split_path, tmp_image)
+            # if not os.path.exists(split_path):
+            #     os.makedirs(split_path)
+            # split_path=osp.join(split_path,repr(i)+".jpg")
+            # cv2.imwrite(split_path, tmp_image)
     print("Acc:{}\tTP:{}\tFP:{}\tFN:{}\tTN:{}".format((correct/total),TP, FP, FN, TN))
+    # for ind, val in enumerate(count):
+    #     print(ind, val)
     return (correct / total)
+    # print(count)
 
 
-
+# test
 if __name__=="__main__":
 
     if args.model=="carnet":
@@ -100,6 +145,9 @@ if __name__=="__main__":
         net=stn_trans_shufflenet()
     elif args.model=='shuf':
         net=torchvision.models.shufflenet_v2_x1_0(pretrained=False,num_classes=2)
+    elif args.model == 'trans_shuf':
+        print("shuf_type:", args.shuf_type)
+        net = trans_shufflenet(shuff_type=args.shuf_type)
     net.load_state_dict({k.replace('module.',''):v for k,v in torch.load(args.path,map_location="cpu").items()})
     if torch.cuda.is_available():
         net.cuda(device)
